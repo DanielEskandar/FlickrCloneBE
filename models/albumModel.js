34a25f551a60897f56503f9747b7577740c821d9
Alia Khalifa <aliaakhalifa12@gmail.com>
2021-04-26 22:57:03 +0200

    P2AG02

---
+++ models/albumModel.js
@@ -0,0 +1,16 @@
+// INCLUDE DEPENDENCIES
+const mongoose = require('mongoose');
+
+// CREATE SCHEMA
+const albumSchema = new mongoose.Schema({
+    albumName:{ type:String, required: true},
+    photos: [{ photoId: mongoose.Schema.ObjectId}],
+    description:String,
+    comments:[{commentId: mongoose.Schema.ObjectId}]
+});
+
+// CREATE MODEL
+const albumModel = mongoose.model('albumModel', albumSchema);
+
+// EXPORT MODEL
+module.exports = albumModel;
---
+++ models/galleryModel.js
@@ -0,0 +1,17 @@
+// INCLUDE DEPENDENCIES
+const mongoose = require('mongoose');
+
+// CREATE SCHEMA
+const gallerySchema = new mongoose.Schema({
+    galleryName:{ type:String, required: true},
+    photos: [{ photoId: mongoose.Schema.ObjectId,remark:String}],
+    description:String,
+    comments:[{commentId: mongoose.Schema.ObjectId}]
+
+});
+
+// CREATE MODEL
+const galleryModel = mongoose.model('galleryModel', gallerySchema);
+
+// EXPORT MODEL
+module.exports = galleryModel;
