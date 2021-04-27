ef81e9068c7f9aaac4ba2f799d4bc5ac0df20868
Daniel Eskandar <danieleskandar@yahoo.com>
2021-04-27 17:03:55 +0200

    Added validations to user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,49 @@
 // INCLUDE DEPENDENCIES
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 // CREATE SCHEMA
 const userSchema = new mongoose.Schema({
-  displayName: String,
-  email: String,
-  password: String,
-  pro: Boolean,
-  firstName: String,
-  lastName: String,
-  age: Number,
+  displayName: {
+    type: String,
+    trim: true,
+    unique: [true, 'Display name must be unique'],
+    required: [true, 'A user must have a display name'],
+    validate: [validator.isAlphanumeric, 'Invalid username'],
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have an email'],
+    unique: [true, 'Email must be unique'],
+    validate: [validator.isEmail, 'Invalid email address'],
+  },
+  password: {
+    type: String,
+    required: [true, 'a user must have a password'],
+    validate: [validator.isStrongPassword, 'Weak password'],
+  },
+  userType: {
+    type: String,
+    required: [true, 'A user must have a user type'],
+    enum: {
+      values: ['normal', 'pro'],
+      message: 'User type is either normal or pro',
+    },
+  },
+  firstName: {
+    type: String,
+    required: [true, 'A user must have a first name'],
+    validate: [validator.isAlphanumeric, 'Invalid first name'],
+  },
+  lastName: {
+    type: String,
+    required: [true, 'A user must have a last name'],
+    validate: [validator.isAlpha, 'Invalid last name'],
+  },
+  age: {
+    type: Number,
+    min: 13,
+  },
   aboutMe: String,
   joinDate: {
     type: Date,
@@ -24,7 +58,13 @@ const userSchema = new mongoose.Schema({
   following: [
     {
       userId: mongoose.Schema.ObjectId,
-      relation: String,
+      relation: {
+        type: String,
+        enum: {
+          values: ['friend', 'family', 'undtermined'],
+          message: 'Relation must be either: friend, family or undetermined',
+        },
+      },
     },
   ],
   blocked: [mongoose.Schema.ObjectId],
