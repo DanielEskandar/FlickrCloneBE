c7f743077ecb434f41334db7f7b6d43d01e11b44
Ahmed Abdulkader <49191231+ahmedabdulkader99@users.noreply.github.com>
2021-05-02 19:41:26 +0200

    Fix Controller Testing and Add Test Cases

--- testing/userController.test.js
+++ testing/userController.test.js
@@ -26,14 +26,72 @@ test('should retrieve display name by id and send response correctly', async () 
   const mReq = { headers: { userid: '608d55c7e512b74ee00791db' } };
   const mRes = {
     status: jest.fn().mockReturnThis(),
-    json: jest.fn(),
+    send: jest.fn().mockReturnThis(),
   };
   await userController.getDispName(mReq, mRes);
   expect(mRes.status).toBeCalledWith(200);
-  expect(mRes.json).toBeCalledWith({
+  expect(mRes.send).toBeCalledWith({
     status: 'success',
     data: {
       displayName: 'DanielEskandar',
+    },
+  });
+});
+
+test('should retrieve display name by id and send response correctly', async () => {
+  const mReq = { headers: { userid: '608d55c7e512b74ee00791dc' } };
+  const mRes = {
+    status: jest.fn().mockReturnThis(),
+    send: jest.fn().mockReturnThis(),
+  };
+  await userController.getDispName(mReq, mRes);
+  expect(mRes.status).toBeCalledWith(200);
+  expect(mRes.send).toBeCalledWith({
+    status: 'success',
+    data: {
+      displayName: 'AliaaKhalifa',
+    },
+  });
+});
+
+test('should retrieve real name by id and send response correctly', async () => {
+  const mReq = {
+    headers: {
+      userid: '608d5450ec00005468607a0c',
+    },
+  };
+  const mRes = {
+    status: jest.fn().mockReturnThis(),
+    send: jest.fn().mockReturnThis(),
+  };
+  await userController.getRealName(mReq, mRes);
+  expect(mRes.status).toBeCalledWith(200);
+  expect(mRes.send).toBeCalledWith({
+    status: 'success',
+    data: {
+      firstName: 'Ahmed',
+      lastName: 'Abdulkader',
+    },
+  });
+});
+
+test('should retrieve real name by id and send response correctly', async () => {
+  const mReq = {
+    headers: {
+      userid: '608d55c7e512b74ee00791dd',
+    },
+  };
+  const mRes = {
+    status: jest.fn().mockReturnThis(),
+    send: jest.fn().mockReturnThis(),
+  };
+  await userController.getRealName(mReq, mRes);
+  expect(mRes.status).toBeCalledWith(200);
+  expect(mRes.send).toBeCalledWith({
+    status: 'success',
+    data: {
+      firstName: 'Mariam',
+      lastName: 'Khashab',
     },
   });
 });
