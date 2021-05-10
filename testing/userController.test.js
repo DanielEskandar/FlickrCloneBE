48f2b3cc4fa970e57b99520c28ba769ed958906f
Daniel Eskandar <danieleskandar@yahoo.com>
2021-05-10 10:21:15 +0200

    Refactored testing data in user module

--- testing/userController.test.js
+++ testing/userController.test.js
@@ -1,6 +1,9 @@
 // INCLUDE CONTROLLER TO TEST
 const userController = require('../controllers/userController.js');
 
+// INCLUDE TEST DATA
+const userTestData = require('./test_data/userTestData.js');
+
 // INCLUDE COMMON TEST HEADERS
 const headers = require('./testCommon.js');
 
@@ -14,12 +17,7 @@ describe('should retrieve display name by id and send response correctly', () =>
     };
     await userController.getDispName(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
-    expect(mRes.json).toBeCalledWith({
-      status: 'success',
-      data: {
-        displayName: 'DanielEskandar',
-      },
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.getDispNameData1);
   });
 
   test('should retrieve display name AliaaKhalifa', async () => {
@@ -30,12 +28,7 @@ describe('should retrieve display name by id and send response correctly', () =>
     };
     await userController.getDispName(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
-    expect(mRes.json).toBeCalledWith({
-      status: 'success',
-      data: {
-        displayName: 'AliaaKhalifa',
-      },
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.getDispNameData2);
   });
 });
 
@@ -53,13 +46,7 @@ describe('should retrieve real name by id and send response correctly', () => {
     };
     await userController.getRealName(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
-    expect(mRes.json).toBeCalledWith({
-      status: 'success',
-      data: {
-        firstName: 'Ahmed',
-        lastName: 'Abdulkader',
-      },
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.getRealNameData1);
   });
 
   test('should retrieve real name Mariam Khashab', async () => {
@@ -74,13 +61,7 @@ describe('should retrieve real name by id and send response correctly', () => {
     };
     await userController.getRealName(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
-    expect(mRes.json).toBeCalledWith({
-      status: 'success',
-      data: {
-        firstName: 'Mariam',
-        lastName: 'Khashab',
-      },
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.getRealNameData2);
   });
 });
 
@@ -98,17 +79,7 @@ describe('should retrieve user info by id and send response correctly', () => {
     };
     await userController.getUserInfo(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
-    expect(mRes.json).toBeCalledWith({
-      status: 'success',
-      data: {
-        email: 'ahmedkader99@mailserver.com',
-        occupation: 'Photographer',
-        hometown: 'Beverly Hills',
-        currentCity: 'California',
-        country: 'United States',
-        joinDate: '2015-10-07T06:09:54.000Z',
-      },
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.getUserInfoData1);
   });
 
   test('should retrieve user info of Daniel Eskandar', async () => {
@@ -123,17 +94,7 @@ describe('should retrieve user info by id and send response correctly', () => {
     };
     await userController.getUserInfo(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
-    expect(mRes.json).toBeCalledWith({
-      status: 'success',
-      data: {
-        email: 'daniel_eskandar99@mailserver.com',
-        occupation: 'Artist',
-        hometown: 'Ile de France',
-        currentCity: 'Paris',
-        country: 'France',
-        joinDate: '2020-11-03T06:10:45.000Z',
-      },
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.getUserInfoData2);
   });
 
   test('should send user not found error', async () => {
@@ -148,10 +109,7 @@ describe('should retrieve user info by id and send response correctly', () => {
     };
     await userController.getUserInfo(mReq, mRes);
     expect(mRes.status).toBeCalledWith(404);
-    expect(mRes.json).toBeCalledWith({
-      status: 'error',
-      message: 'No user is found by that ID',
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.userNotFound);
   });
 });
 
@@ -165,21 +123,7 @@ describe('should retrieve limits by id and send response correctly', () => {
     };
     await userController.getLimits(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
-    expect(mRes.json).toBeCalledWith({
-      status: 'success',
-      data: {
-        limits: {
-          photos: {
-            maxdisplaypx: 1024,
-            maxupload: 15728640,
-          },
-          videos: {
-            maxduration: 90,
-            maxupload: 15728640,
-          },
-        },
-      },
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.getLimitsData1);
   });
 
   test('should retrieve limits AliaaKhalifa', async () => {
@@ -190,21 +134,7 @@ describe('should retrieve limits by id and send response correctly', () => {
     };
     await userController.getLimits(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
-    expect(mRes.json).toBeCalledWith({
-      status: 'success',
-      data: {
-        limits: {
-          photos: {
-            maxdisplaypx: 1024,
-            maxupload: 15728640,
-          },
-          videos: {
-            maxduration: 90,
-            maxupload: 15728640,
-          },
-        },
-      },
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.getLimitsData2);
   });
 });
 
@@ -218,59 +148,7 @@ describe('should retrieve following list of a user and send response correctly',
     };
     await userController.getFollowing(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
-    expect(mRes.json).toBeCalledWith({
-      status: 'success',
-      count: 4,
-      data: {
-        _id: '608d55c7e512b74ee00791de',
-        following: [
-          {
-            _id: '60914e1ba87d736f3c382ed6',
-            user: {
-              _id: '608d55c7e512b74ee00791db',
-              displayName: 'DanielEskandar',
-              firstName: 'Daniel',
-              lastName: 'Eskandar',
-            },
-            relation: 'undetermined',
-            followDate: '2021-05-04T12:16:24.255Z',
-          },
-          {
-            _id: '60914e1ba87d736f3c382ed7',
-            user: {
-              _id: '608d5450ec00005468607a0c',
-              displayName: 'AhmedAbdulkader99',
-              firstName: 'Ahmed',
-              lastName: 'Abdulkader',
-            },
-            relation: 'undetermined',
-            followDate: '2021-05-04T12:17:24.255Z',
-          },
-          {
-            _id: '60914e1ba87d736f3c382ed8',
-            user: {
-              _id: '608d55c7e512b74ee00791dc',
-              displayName: 'AliaaKhalifa',
-              firstName: 'Aliaa',
-              lastName: 'Khalifa',
-            },
-            relation: 'undetermined',
-            followDate: '2021-05-04T12:18:24.255Z',
-          },
-          {
-            _id: '60914e1ba87d736f3c382ed9',
-            user: {
-              _id: '608d55c7e512b74ee00791dd',
-              displayName: 'MariamKhashab',
-              firstName: 'Mariam',
-              lastName: 'Khashab',
-            },
-            relation: 'undetermined',
-            followDate: '2021-05-04T12:19:24.255Z',
-          },
-        ],
-      },
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.getFollowingData1);
   });
 
   test('should retrieve following list of AhmedAbdulkader99', async () => {
@@ -281,26 +159,7 @@ describe('should retrieve following list of a user and send response correctly',
     };
     await userController.getFollowing(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
-    expect(mRes.json).toBeCalledWith({
-      status: 'success',
-      count: 1,
-      data: {
-        _id: '608d5450ec00005468607a0c',
-        following: [
-          {
-            _id: '60914e1ba87d736f3c382ece',
-            user: {
-              _id: '608d55c7e512b74ee00791dc',
-              displayName: 'AliaaKhalifa',
-              firstName: 'Aliaa',
-              lastName: 'Khalifa',
-            },
-            relation: 'friend',
-            followDate: '2021-05-04T12:12:24.255Z',
-          },
-        ],
-      },
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.getFollowingData2);
   });
 
   test('should send user not found error', async () => {
@@ -311,10 +170,7 @@ describe('should retrieve following list of a user and send response correctly',
     };
     await userController.getFollowing(mReq, mRes);
     expect(mRes.status).toBeCalledWith(404);
-    expect(mRes.json).toBeCalledWith({
-      status: 'error',
-      message: 'No user is found by that ID',
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.userNotFound);
   });
 });
 
@@ -328,27 +184,7 @@ describe('should retrieve blocked list of a user and send response correctly', (
     };
     await userController.getBlocked(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
-    expect(mRes.json).toBeCalledWith({
-      status: 'success',
-      count: 2,
-      data: {
-        blocked: [
-          {
-            _id: '608d5450ec00005468607a0c',
-            displayName: 'AhmedAbdulkader99',
-            firstName: 'Ahmed',
-            lastName: 'Abdulkader',
-          },
-          {
-            _id: '608d55c7e512b74ee00791dc',
-            displayName: 'AliaaKhalifa',
-            firstName: 'Aliaa',
-            lastName: 'Khalifa',
-          },
-        ],
-        _id: '608d55c7e512b74ee00791db',
-      },
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.getBlockedData1);
   });
 
   test('should retrieve blocked list of AliaaKhalifa', async () => {
@@ -359,14 +195,7 @@ describe('should retrieve blocked list of a user and send response correctly', (
     };
     await userController.getBlocked(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
-    expect(mRes.json).toBeCalledWith({
-      status: 'success',
-      count: 0,
-      data: {
-        blocked: [],
-        _id: '608d55c7e512b74ee00791dc',
-      },
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.getBlockedData2);
   });
 });
 
@@ -653,23 +482,7 @@ describe('should retrieve notification settings by id and send response correctl
     };
     await userController.getNotificationSettings(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
-    expect(mRes.json).toBeCalledWith({
-      status: 'success',
-      data: {
-        notificationSettings: {
-          notifMail: {
-            invites: true,
-            contact: true,
-            messages: true,
-            reminders: true,
-          },
-          activityMail: {
-            you: true,
-            contacts: true,
-          },
-        },
-      },
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.getNotificationSettingsData1);
   });
 
   test('should retrieve notification settings of AliaaKhalifa', async () => {
@@ -680,23 +493,7 @@ describe('should retrieve notification settings by id and send response correctl
     };
     await userController.getNotificationSettings(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
-    expect(mRes.json).toBeCalledWith({
-      status: 'success',
-      data: {
-        notificationSettings: {
-          notifMail: {
-            invites: true,
-            contact: true,
-            messages: true,
-            reminders: true,
-          },
-          activityMail: {
-            you: true,
-            contacts: true,
-          },
-        },
-      },
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.getNotificationSettingsData2);
   });
 });
 
@@ -710,46 +507,7 @@ describe('should retrieve privacy settings by id and send response correctly', (
     };
     await userController.getPrivacySettings(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
-    expect(mRes.json).toBeCalledWith({
-      status: 'success',
-      data: {
-        privacySettings: {
-          global: {
-            infoVisiblity: {
-              email: 2,
-              name: 1,
-              currentCity: 1,
-            },
-            downloadPerm: 1,
-            largestImgSize: 0,
-            allowShare: 1,
-            allowTag: 1,
-            allowGalleryAdd: true,
-            hideEXIF: false,
-            hidePhotoSearch: false,
-            hideProfileSearch: false,
-          },
-          defaults: {
-            perms: {
-              see: 1,
-              comment: 1,
-              addNotes: 2,
-            },
-            license: 0,
-            mapVisible: 1,
-            importEXIF: true,
-            safetyLevel: 1,
-            contentType: 1,
-          },
-          filters: {
-            search: {
-              safeSearch: true,
-              content: 1,
-            },
-          },
-        },
-      },
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.getPrivacySettingsData1);
   });
 
   test('should retrieve privacy settings of AliaaKhalifa', async () => {
@@ -760,45 +518,6 @@ describe('should retrieve privacy settings by id and send response correctly', (
     };
     await userController.getPrivacySettings(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
-    expect(mRes.json).toBeCalledWith({
-      status: 'success',
-      data: {
-        privacySettings: {
-          global: {
-            infoVisiblity: {
-              email: 2,
-              name: 1,
-              currentCity: 1,
-            },
-            downloadPerm: 1,
-            largestImgSize: 0,
-            allowShare: 1,
-            allowTag: 1,
-            allowGalleryAdd: true,
-            hideEXIF: false,
-            hidePhotoSearch: false,
-            hideProfileSearch: false,
-          },
-          defaults: {
-            perms: {
-              see: 1,
-              comment: 1,
-              addNotes: 2,
-            },
-            license: 0,
-            mapVisible: 1,
-            importEXIF: true,
-            safetyLevel: 1,
-            contentType: 1,
-          },
-          filters: {
-            search: {
-              safeSearch: true,
-              content: 1,
-            },
-          },
-        },
-      },
-    });
+    expect(mRes.json).toBeCalledWith(userTestData.getPrivacySettingsData2);
   });
 });
